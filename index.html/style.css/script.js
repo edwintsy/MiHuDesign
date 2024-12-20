ae582de9c4fa5b82b8f07e7617883565ad459975
edwintsy <siyong07007@gmail.com>
2024-12-21 01:54:47 +0800

    1220

--- index.html/style.css/script.js
+++ index.html/style.css/script.js
@@ -1,20 +1,19 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const dynamicElements = document.querySelectorAll(".dynamic-text");
-  
-    const observerOptions = {
-      threshold: 0.1, // 元素出现 10% 时触发
-    };
-  
-    const observer = new IntersectionObserver((entries) => {
-      entries.forEach((entry) => {
-        if (entry.isIntersecting) {
-          entry.target.classList.add("visible");
-        } else {
-          entry.target.classList.remove("visible");
-        }
-      });
-    }, observerOptions);
-  
-    dynamicElements.forEach((el) => observer.observe(el));
-  });
-  
+  const dynamicElements = document.querySelectorAll(".dynamic-text");
+
+  const observerOptions = {
+    threshold: 0.1, // 视口显示 10% 时触发
+  };
+
+  const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("visible");
+      } else {
+        entry.target.classList.remove("visible");
+      }
+    });
+  }, observerOptions);
+
+  dynamicElements.forEach((el) => observer.observe(el));
+});
