06505caf0f10e2e28c713f817a3e56de85810db6
edwintsy <siyong07007@gmail.com>
2024-12-24 21:25:32 +0800

    20241225

--- index.html/style.css/script.js
+++ index.html/style.css/script.js
@@ -1,17 +1,13 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const dynamicElements = document.querySelectorAll(".dynamic-text");
-
-  const observerOptions = {
-    threshold: 0.5, // 元素至少有 50% 进入视口时触发
-  };
-
   const observer = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
       if (entry.isIntersecting) {
         entry.target.classList.add("visible");
       }
     });
-  }, observerOptions);
+  });
 
-  dynamicElements.forEach((el) => observer.observe(el));
+  document.querySelectorAll(".dynamic-text").forEach((el) => {
+    observer.observe(el);
+  });
 });
