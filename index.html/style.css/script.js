4d37b189c31a4cb791c2ed8646ac5b5be9168e15
edwintsy <siyong07007@gmail.com>
2024-12-24 21:12:22 +0800

    20241225

--- index.html/style.css/script.js
+++ index.html/style.css/script.js
@@ -2,15 +2,13 @@ document.addEventListener("DOMContentLoaded", () => {
   const dynamicElements = document.querySelectorAll(".dynamic-text");
 
   const observerOptions = {
-    threshold: 0.1, // 视口显示 10% 时触发
+    threshold: 0.1, // 可见区域的 10% 时触发
   };
 
   const observer = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
       if (entry.isIntersecting) {
-        entry.target.classList.add("visible");
-      } else {
-        entry.target.classList.remove("visible");
+        entry.target.classList.add("visible"); // 添加 visible 类
       }
     });
   }, observerOptions);
